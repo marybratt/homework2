97e974ae1f010a3a5286e046bce73f2c8e7c34c9
marybratt <45605582+marybratt@users.noreply.github.com>
2018-12-11 12:52:32 -0800

    add comment to show that pulled down from github

--- newProject.java
+++ newProject.java
@@ -5,6 +5,7 @@ public class newProject {
 	public static void main(String[] args) {
 		// Trying to upload from scratch to new repository
 		System.out.println("this is a test");
+		// add a comment to show that this is the one pulled from homework2
 	}
 
 }
