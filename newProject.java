3377760dc53de92a44eb5814f691b6041d3d8541
marybratt <mbratt56@gmail.com>
2018-12-11 13:03:47 -0800

    bringing back up

--- newProject.java
+++ newProject.java
@@ -6,7 +6,11 @@ public class newProject {
 		// Trying to upload from scratch to new repository
 		System.out.println("this is a test");
 		// add a comment to show that this is the one pulled from homework2
+		
+		// now adding to show that upload again
+		System.out.println("wow! it worked");
 	}
 
 }
+# homework2
 # homework2
