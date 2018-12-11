732a8568ed151e339bf2882c229c7f10e6684e3e
marybratt <mbratt56@gmail.com>
2018-12-11 12:48:50 -0800

    first commit

---
+++ newProject.java
@@ -0,0 +1,11 @@
+package newProject;
+
+public class newProject {
+
+	public static void main(String[] args) {
+		// Trying to upload from scratch to new repository
+		System.out.println("this is a test");
+	}
+
+}
+# homework2
